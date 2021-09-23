16678e569443092348bb7301caf75f9e81b51802
markovical <aca997@gmail.com>
2021-09-23 19:18:43 +0200

    Konekcioni parametri za bazu uradjeni putem json-a

---
+++ MavenFitnessServer/src/main/java/db/DBParameters.java
@@ -0,0 +1,28 @@
+package db;
+
+public class DBParameters {
+	public String connectionString;
+	public String username;
+	public String password;
+	public DBParameters() {
+		
+	}
+	public String getConnectionString() {
+		return connectionString;
+	}
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
